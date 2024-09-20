051aedc26b5aa2ecea21e2fff301349305201935
Anastasiia <troyanovskaya2003@gmail.com>
2024-09-20 01:32:41 +0100

    build

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { LogInPageComponent } from './components/log-in-page/log-in-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LogInPageComponent}
+];
